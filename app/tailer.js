adb9a45cf1115328d621d7853b3d7e0856c87e24
Bernard Labno <bernard.labno@pjwstk.edu.pl>
2015-11-20 15:41:08 +0100

    Solution

--- app/tailer.js
+++ app/tailer.js
@@ -1,5 +1,27 @@
 'use strict';
 
-module.exports = function tailer(db, collectionName, onData) {
+var Promise = require('bluebird');
 
+module.exports = function (db, collectionName, onData)
+{
+    var command = {convertToCapped: null, size: 100};
+    command.convertToCapped = collectionName;
+    return new Promise(function (resolve, reject)
+    {
+        db.command(command, function (err)
+        {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
+    }).delay(1000).then(function ()
+    {
+        return db.collection(collectionName).find({})
+                .addCursorFlag('tailable', true)
+                .addCursorFlag('awaitData', true)
+                .addCursorFlag('noCursorTimeout', true)
+                .on('data', onData);
+    });
 };
