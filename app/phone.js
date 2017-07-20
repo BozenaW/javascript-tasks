c2d88f1742e90b9948ef86c98b850096f8a1a200
aniaw <ania_w@outlook.com>
2017-07-20 14:57:49 +0200

    solution

--- app/phone.js
+++ app/phone.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     search: function search(phoneCollection) {
-        return phoneCollection.find({$text: {$search:'\'Share photos videos\' and movies'}}).project({name: 1, description: 1, _id: 0}).toArray();
+        return phoneCollection.find({$text: {$search:'\"Share photos videos\" and movies'}}).project({name: 1, description: 1, _id: 0}).toArray();
     }
 };
